85f4e660621a0e0a551fba2aa6c44f8f3cf366ef
Dhruvpal77 <67869389+Dhruvpal77@users.noreply.github.com>
2020-11-11 22:32:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,98 @@
-<html>
-<h1>welcome</h1>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <title> Dhruv Restaurant</title>
+    <style>
+      p{
+        color: rgba(255, 255, 0, 0.993);
+      }
+    </style>
+  </head>
+  <body style="background-color: black;">
+    <div class="container-fluid">
+      <div class="container text-center shadow-lg p-3 mb-5  rounded" style="background-color: black; color: white;">
+        <img style="border-radius: 50%;" src="https://m.hindustantimes.com/rf/image_size_444x250/HT/p2/2020/06/23/Pictures/_a468a0a2-b572-11ea-b8d7-dab47869eae4.jpg" alt="food" style="width:100%;height:500px;">
+        <h1 class="display-1" >Dhruv Banna Resto</h1>      
+        <p class="display-4" style="font-size:35px ;">Where food matters</p>
+      </div>
+    </div>
+    <div class="container" style="background-color:rgb(27, 27, 27);color: beige; border-radius: 15px ; height: 900px;">
+      <div >
+        <div class="row" style="padding-left: 40px;">                                     
+        <div class="col-sm-4" >                 
+          <div style="padding-top: 25px; ">
+            <img src="https://www.thespruceeats.com/thmb/X6mg_2VBCQQ2X8VrLcPTf8_4ce0=/2733x2050/smart/filters:no_upscale()/chinese-take-out-472927590-57d31fff3df78c5833464e7b.jpg" class="img-thumbnail" alt="Cinque Terre" width="304" height="236">
+          </div>
+        </div>                  
+        <div class="col-sm-4">
+          <div style="padding-top: 25px;">
+            <img src="https://ethnomed.org/wp-content/uploads/2020/01/chinese-food-omefrans.jpg" class="img-thumbnail" alt="Cinque Terre" width="304" height="236">
+          </div>
+        </div>   
+        <div class="col-sm-4">
+          <div style="padding-top: 25px;">
+            <img src="https://media-cdn.tripadvisor.com/media/photo-s/0e/67/fc/fc/gunjan-chinese-food.jpg" class="img-thumbnail" alt="Cinque Terre" width="304" height="236">
+          </div>
+        </div>                
+    </div>
+        <hr style=" height:1px; background-color :#d87093;">
+        <div class="text-center">
+                <div class="m-5">           
+                  <h3 class="display-4">Menu</h3>
+                </div>
+                    
+                <div class="input">
+                    <select name="menulist" id="menulist"></select>
+                    </div>
+                    <div class="menulists">
+                    <h4 class="lead">Short name:</h4>
+                    <p class="short__name lead"></p>
+                    <h4 class="lead">Name :</h4>
+                    <p class="item_name"></p>
+                    <h4 class="lead">Description:</h4>
+                    <p class="desc"></p>
+                    <h4 class="lead" >Price :</h4>
+                      <h4 class="lead" >Small Dish :</h4>
+                      <p class="small_dish"></p>
+                      <h4 class="lead" >Large Dish :</h4>
+                        <p class="large_dish"></p>
+                  </div>
+                  </div>
+      </div>
+          
+    </div>
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+    <script src="script.js"></script>
+  </body>
+  <footer class="text-center m-5 pb-5">
+    <hr>
+    <code >Made By Dhruv Banna</code>
+  </footer>
 </html>
